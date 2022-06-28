c1c8ac95697f03099809527415a714c5514816d1
welcomemncube <welcomemcube@gmail.com>
2022-06-28 10:30:23 +0200

    Update simple-node-app

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const dotenv = require("dotenv");
 const app = express();
 const PORT = process.env.PORT;
 
+//Create a simple get route
 app.get("/", (req, res) => {
  res.send("Hello World");
 });
 
 
 
-
+//Listen on PORT 3000
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
